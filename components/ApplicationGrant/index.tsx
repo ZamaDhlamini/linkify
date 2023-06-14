f58860bc771eca8cdd4e8bc9e5c5a6efa9635cf8
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-06-14 16:59:16 +0200

    finished off the form for the apply grant

--- components/ApplicationGrant/index.tsx
+++ components/ApplicationGrant/index.tsx
@@ -1,50 +1,76 @@
-import { Form, Input, Button } from 'antd';
-import 'antd/dist/antd.css';
+import { Form, Input, Button, Row, Col } from 'antd';
+import { useState } from 'react';
+import styles from './Application.module.css';
+import Navbar from '../NavBar';
 
-const MyForm = () => {
+const GrantForm = () => {
+    const [isSaveClicked, setIsSaveClicked] = useState(false);
+
   const onFinish = (values) => {
     console.log('Form values:', values);
+    setIsSaveClicked(true);
   };
 
   return (
-    <Form onFinish={onFinish}>
-      <Form.Item label="ID Number" name="idNumber" rules={[{ required: true, message: 'Please enter your ID number' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Surname" name="surname" rules={[{ required: true, message: 'Please enter your surname' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Full Name" name="fullName" rules={[{ required: true, message: 'Please enter your full name' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Email Address" name="email" rules={[{ required: true, type: 'email', message: 'Please enter a valid email address' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Cellphone Number" name="cellphone" rules={[{ required: true, message: 'Please enter your cellphone number' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Street Name" name="street" rules={[{ required: true, message: 'Please enter the street name' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="City" name="city" rules={[{ required: true, message: 'Please enter the city' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Province" name="province" rules={[{ required: true, message: 'Please enter the province' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Postal Code" name="postalCode" rules={[{ required: true, message: 'Please enter the postal code' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Save
-        </Button>
-        <Button type="primary" htmlType="button" style={{ marginLeft: 8 }}>
-          Next Page
-        </Button>
-      </Form.Item>
-    </Form>
+    <><Navbar /><Form onFinish={onFinish}>
+        <div className={styles.heading1}>
+        <h1>Application for Child Support Grant</h1>
+        </div>
+          <Row gutter={16}>
+              <Col span={12}>
+                <div className={styles.container1}>
+                  <Form.Item label="ID Number" name="idNumber" rules={[{ required: true, message: 'Please enter your ID number' }]}>
+                      <Input />
+                  </Form.Item>
+                  <Form.Item label="Surname" name="surname" rules={[{ required: true, message: 'Please enter your surname' }]}>
+                      <Input />
+                  </Form.Item>
+                  <Form.Item label="Full Name" name="fullName" rules={[{ required: true, message: 'Please enter your full name' }]}>
+                      <Input />
+                  </Form.Item>
+                  <Form.Item label="Email Address" name="email" rules={[{ required: true, type: 'email', message: 'Please enter a valid email address' }]}>
+                      <Input />
+                  </Form.Item>
+                  <Form.Item label="Cellphone Number" name="cellphone" rules={[{ required: true, message: 'Please enter your cellphone number' }]}>
+                      <Input />
+                  </Form.Item>
+                </div>
+              </Col>
+              <Col span={12}>
+                <div className={styles.container2}>
+                  <Form.Item label="Street Name" name="street" rules={[{ required: true, message: 'Please enter the street name' }]}>
+                      <Input />
+                  </Form.Item>
+                  <Form.Item label="City" name="city" rules={[{ required: true, message: 'Please enter the city' }]}>
+                      <Input />
+                  </Form.Item>
+                  <Form.Item label="Province" name="province" rules={[{ required: true, message: 'Please enter the province' }]}>
+                      <Input />
+                  </Form.Item>
+                  <Form.Item label="Postal Code" name="postalCode" rules={[{ required: true, message: 'Please enter the postal code' }]}>
+                      <Input />
+                  </Form.Item>
+                </div>
+              </Col>
+          </Row>
+          <Form.Item>
+              <div className={styles.button1}>
+                  <Button type="primary" htmlType="submit">
+                      Save
+                  </Button>
+              </div>
+              {isSaveClicked && ( // Display the next page button if save button is clicked
+                  <Button type="primary" htmlType="button" style={{ marginLeft: 8 }}>
+                      Next Page
+                  </Button>
+              )}
+          </Form.Item>
+          <div className={styles.imageLogo}>
+        <img src="/coatofarms.png" alt="Banner box" />
+      </div>
+      </Form></>
+      
   );
 };
 
-export default MyForm;
+export default GrantForm;
--- components/SignUp/index.tsx
+++ components/SignUp/index.tsx
@@ -16,63 +16,73 @@ const SignUpForm = () => {
         <h2>Sign Up</h2>
       </div> */}
       <div className={styles.container}>
-      <Form name="signup" onFinish={onFinish}>
-        <Form.Item
-          name="name"
-          rules={[{ required: true, message: 'Please enter your name!' }]}
-          className={styles.formItem}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Name" />
-        </Form.Item>
-        <Form.Item
-          name="surname"
-          rules={[{ required: true, message: 'Please enter your surname!' }]}
-          className={styles.formItem}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Surname" />
-        </Form.Item>
-        <Form.Item
-          name="identityNumber"
-          rules={[{ required: true, message: 'Please enter your identity number!' }]}
-          className={styles.formItem}
-        >
-          <Input prefix={<SolutionOutlined />} placeholder="Identity Number" />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          rules={[
-            { required: true, message: 'Please enter your email!' },
-            { type: 'email', message: 'Please enter a valid email address!' },
-          ]}
-          className={styles.formItem}
-        >
-          <Input prefix={<MailOutlined />} placeholder="Email" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please enter your password!' }]}
-          className={styles.formItem}
-        >
-          <Input.Password prefix={<LockOutlined />} placeholder="Password" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Sign Up
-          </Button>
-        </Form.Item>
-      </Form>
+        <Form name="signup" onFinish={onFinish}>
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: 'Please enter your name!' }]}
+            className={styles.formItem}
+          >
+            <div className={styles.inputLabel}>Name:</div>
+            <Input prefix={<UserOutlined />} placeholder="Enter your name" />
+          </Form.Item>
+          <Form.Item
+            name="surname"
+            rules={[{ required: true, message: 'Please enter your surname!' }]}
+            className={styles.formItem}
+          >
+            <div className={styles.inputLabel}>Surname:</div>
+            <Input prefix={<UserOutlined />} placeholder="Enter your surname" />
+          </Form.Item>
+          <Form.Item
+            name="identityNumber"
+            rules={[{ required: true, message: 'Please enter your identity number!' }]}
+            className={styles.formItem}
+          >
+            <div className={styles.inputLabel}>Identity Number:</div>
+            <Input prefix={<SolutionOutlined />} placeholder="Enter your identity number" />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[
+              { required: true, message: 'Please enter your email!' },
+              { type: 'email', message: 'Please enter a valid email address!' },
+            ]}
+            className={styles.formItem}
+          >
+            <div className={styles.inputLabel}>Email:</div>
+            <Input prefix={<MailOutlined />} placeholder="Enter your email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please enter your password!' }]}
+            className={styles.formItem}
+          >
+            <div className={styles.inputLabel}>Password:</div>
+            <Input.Password prefix={<LockOutlined />} placeholder="Enter your password" />
+          </Form.Item>
+          <Form.Item>
+            <div className={styles.button}>
+            <Button type="primary" htmlType="submit">
+              Sign Up
+            </Button>
+            </div>
+          </Form.Item>
+        </Form>
       </div>
       <div className={styles.image}>
-            <img src="/banner.png" alt="Banner box" />
-            </div>
-            <div className={styles.imageLogo}>
-            <img src="/coatofarms.png" alt="Banner box" />
-            </div>
-            <div className={styles.paragraph1}>
-              <div className={styles.scrollText}>
-            <p>The Republic of South Africa is a parliamentary republic with a three-tier system of government and an independent judiciary, operating in a parliamentary system.</p>
-              </div>
-            </div>
+        <img src="/banner.png" alt="Banner box" />
+      </div>
+      <div className={styles.imageLogo}>
+        <img src="/coatofarms.png" alt="Banner box" />
+      </div>
+      <div className={styles.paragraph1}>
+        <div className={styles.scrollText}>
+          <p>
+            The Republic of South Africa is a parliamentary republic with a three-tier system of government and an
+            independent judiciary, operating in a parliamentary system.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
