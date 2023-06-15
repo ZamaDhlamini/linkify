3fdb5120c78878bf98c7f2ff713eab13f33d29be
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-06-15 16:32:19 +0200

    tried to delete ran into issues

---
+++ pages/BranchMaps/providers/person/action.tsx
@@ -0,0 +1,16 @@
+import { createAction } from "redux-actions";
+import { ILogin, IUser, IUsersStateContext } from "./context";
+
+export enum UsersActionEnums{
+    LoginUserRequest = 'LOGIN',
+    CreateUserRequest = 'CREATE',
+    CreateUserSuccess = 'CREATE_USER_SUCCESS',
+    CreateUserError = 'CREATE_USER_ERROR',
+    LogoutUser = 'LOGOUT_USER',
+}
+
+export const LoginUserRequestAction = createAction<IUsersStateContext, Array<ILogin>>(UsersActionEnums.LoginUserRequest, (Login) => ({Login}));
+export const CreateUserRequestAction = createAction<IUsersStateContext, IUser>(UsersActionEnums.CreateUserRequest, (UserCreated) => ({UserCreated}));
+export const CreateUserSuccessAction = createAction<IUsersStateContext, IUser>(UsersActionEnums.CreateUserRequest, (UserCreated) => ({UserCreated}));
+export const CreateUserErrorAction = createAction<IUsersStateContext, string>(UsersActionEnums.CreateUserError, (errorMessage) => ({errorMessage}));
+export const LogoutUserAction = createAction<IUsersStateContext>(UsersActionEnums.LogoutUser);
---
+++ pages/BranchMaps/providers/person/context.tsx
@@ -0,0 +1,38 @@
+import { createContext } from "react";
+
+export interface IUser{
+    IDNumber: string;
+    Name: string;
+    LastName: string;
+    Password: string;
+    PhoneNumber: string;
+    EmailAddress: string;
+}
+
+export interface ILogin{
+    id?: string;
+    username?: string;
+    password?: string;
+    email?:string
+}
+
+export interface IUsersStateContext{
+    readonly UserCreated?: IUser;
+    readonly Login?: Array<ILogin>;
+    readonly errorMessage?: string;
+    readonly logout?: boolean;
+}
+
+export const  INITIAL_STATE: IUsersStateContext = {}
+export interface IUsersActionContext{
+    createUser?:(payload:IUser) => void;
+    login?:(payload: ILogin) => void;
+    logout?: () => void;
+
+}
+
+const UserContext = createContext<IUsersStateContext>(INITIAL_STATE);
+export const USersStateContext = createContext<IUsersStateContext>({});
+export const UsersActionsContext = createContext<IUsersActionContext | undefined>(undefined);
+
+export {UserContext};
