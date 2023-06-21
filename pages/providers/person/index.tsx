1ca4d1fe77901d2d4e3bda588d27bca120340a74
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-06-21 09:24:55 +0200

    provider changes

--- pages/providers/person/index.tsx
+++ pages/providers/person/index.tsx
@@ -17,7 +17,7 @@ const UsersProvider: FC<PropsWithChildren<any>> = ({children}) => {
 
   const createUserMutation = useMutate({
     verb: 'POST',
-    path: 'Person/Create',
+    path: 'User/Create',
   });
 
   const { refetch: getPersonById, error: personByIdError, loading: isLoadingPerson, data: person } = useGet({
