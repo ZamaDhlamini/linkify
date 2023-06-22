3251e975d4624c1389516b51f6c52cf549b5804e
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-06-22 18:06:41 +0200

    styled the form and the upload too

--- components/ApplicationGrant/index.tsx
+++ components/ApplicationGrant/index.tsx
@@ -2,10 +2,11 @@ import { Form, Input, Button, Row, Col } from 'antd';
 import { useState } from 'react';
 import styles from './ApplicationGrant.module.css';
 import Navbar from '../NavBar';
+import Link from 'next/link';
 
 const GrantForm = () => {
-    const [isSaveClicked, setIsSaveClicked] = useState(false);
-    const [isSaved, setIsSaved] = useState(false);
+  const [isSaveClicked, setIsSaveClicked] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
 
   const onFinish = (values) => {
     console.log('Form values:', values);
@@ -14,104 +15,118 @@ const GrantForm = () => {
   };
 
   return (
-    <><Navbar /><Form onFinish={onFinish}>
+    <>
+      <Navbar />
+      <Form onFinish={onFinish} >
         <div className={styles.heading1}>
-        <h1>Application for Child Support Grant</h1>
+          <h1>Application for Child Support Grant</h1>
         </div>
-          <Row gutter={[16, 16]}>
-              <Col span={12}>
-                <div className={styles.container1}>
-                <Form.Item
-                label="ID Number"
-                name="idNumber"
-                rules={[{ required: true, message: 'Please enter your ID number' }]}
+
+        <Row gutter={[16, 16]}>
+            <div className={styles.coloumn1}>
+          <Col span={8}>
+            <Form.Item
+              label="ID Number"
+              name="idNumber"
+              labelCol={{span:15}}
+              rules={[{ required: true, message: 'Please enter your ID number' }]}
+            >
+              <Input className={styles.input} />
+            </Form.Item>
+            <Form.Item
+              label="Surname"
+              name="surname"
+              labelCol={{span:15}}
+              rules={[{ required: true, message: 'Please enter your surname' }]}
+            >
+              <Input className={styles.input} />
+            </Form.Item>
+            <Form.Item
+              label="Full Name"
+              name="fullName"
+              labelCol={{span:15}}
+              rules={[{ required: true, message: 'Please enter your full name' }]}
               >
-                <Input className={styles.input} />
-              </Form.Item>
-              <Form.Item
-                label="Surname"
-                name="surname"
-                rules={[{ required: true, message: 'Please enter your surname' }]}
-              >
-                <Input className={styles.input} />
-              </Form.Item>
-              <Form.Item
-                label="Full Name"
-                name="fullName"
-                rules={[{ required: true, message: 'Please enter your full name' }]}
-              >
-                <Input className={styles.input} />
-              </Form.Item>
-              <Form.Item
-                label="Email Address"
-                name="email"
-                rules={[{ required: true, type: 'email', message: 'Please enter a valid email address' }]}
-              >
-                <Input className={styles.input} />
-              </Form.Item>
-              <Form.Item
-                label="Cellphone Number"
-                name="cellphone"
-                rules={[{ required: true, message: 'Please enter your cellphone number' }]}
-              >
-                <Input className={styles.input} />
-              </Form.Item>
+              <Input className={styles.input} />
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              labelCol={{span:15}}
+              rules={[{ required: true, type: 'email', message: 'Please enter a valid email address' }]}
+            >
+              <Input className={styles.input} />
+            </Form.Item>
+            <Form.Item
+
+            label="Cell Number"
+            name="cellphone"
+            labelCol={{span:15}}
+            rules={[{ required: true, message: 'Please enter your cellphone number' }]}
+>
+              <Input className={styles.input} />
+            </Form.Item>
+          </Col>
                 </div>
-              </Col>
-              <Col span={12}>
-                <div className={styles.container2}>
-                <Form.Item
-                label="Street Name"
-                name="street"
-                rules={[{ required: true, message: 'Please enter the street name' }]}
+          <div className={styles.column2}>
+          <Col span={8}>
+            <Form.Item
+              label="Street Name"
+              name="street"
+              rules={[{ required: true, message: 'Please enter the street name' }]}
+            >
+              <Input className={styles.input} />
+            </Form.Item>
+            <Form.Item
+              label="City"
+              name="city"
+              labelCol={{span:16}}
+              rules={[{ required: true, message: 'Please enter the city' }]}
+            >
+              <Input className={styles.input} />
+            </Form.Item>
+            <Form.Item
+              label="Province"
+              name="province"
+              labelCol={{span:16}}
+              rules={[{ required: true, message: 'Please enter the province' }]}
+            >
+              <Input className={styles.input} />
+            </Form.Item>
+            <Form.Item
+              label="Postal Code"
+              name="postalCode"
+              labelCol={{span:16}}
+              rules={[{ required: true, message: 'Please enter the postal code' }]}
               >
-                <Input className={styles.input} />
-              </Form.Item>
-              <Form.Item
-                label="City"
-                name="city"
-                rules={[{ required: true, message: 'Please enter the city' }]}
-              >
-                <Input className={styles.input} />
-              </Form.Item>
-              <Form.Item
-                label="Province"
-                name="province"
-                rules={[{ required: true, message: 'Please enter the province' }]}
-              >
-                <Input className={styles.input} />
-              </Form.Item>
-              <Form.Item
-                label="Postal Code"
-                name="postalCode"
-                rules={[{ required: true, message: 'Please enter the postal code' }]}
-              >
-                <Input className={styles.input} />
-              </Form.Item>
+              <Input className={styles.input} />
+            </Form.Item>
+          </Col>
                 </div>
-              </Col>
-          </Row>
-          <Form.Item>
-              <div>
-                  <Button className={styles.button1} htmlType="submit">
-                      Save
-                  </Button>
-              </div>
-              {isSaved && <p>Saved successfully</p>}
-              <div className={styles.buttonContainer}>
-              {isSaveClicked && ( // Display the next page button if save button is clicked
-                  <Button htmlType="button" style={{ marginLeft: 8 }} className={styles.button2}>
-                      Next Page
-                  </Button>
-              )}
-              </div>
-          </Form.Item>
-          <div className={styles.imageLogo}>
+        </Row>
+        <Form.Item>
+          <div>
+            <Button className={styles.button1} htmlType="submit">
+              Save
+            </Button>
+          </div>
+          {isSaved && <p>Saved successfully</p>}
+          <div className={styles.buttonContainer}>
+            {isSaveClicked && ( // Display the next page button if save button is clicked
+              <Button htmlType="button" style={{ marginLeft: 8 }} className={styles.button2}>
+                Next Page
+              </Button>
+            )}
+          </div>
+        </Form.Item>
+      </Form>
+      {/* <div className={styles.imageLogo}>
         <img src="/coatofarms.png" alt="Banner box" />
-        <h5>Stuck?Click here for help</h5>
-      </div>
-      </Form></>
-      
+        <Link href="/KnowledgeBase">
+          <h5>Stuck? Click here for help</h5>
+        </Link>
+      </div> */}
+    </>
   );
 };
 
--- components/Upload/index.tsx
+++ components/Upload/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import styles from './Upload.module.css';
 
 const documentTypes = [
   'Bank Letter',
@@ -30,26 +31,50 @@ const UploadPage = () => {
     console.log(fileList);
   };
 
+  
+
+  const documentTypesRow1 = documentTypes.slice(0, 6);
+  const documentTypesRow2 = documentTypes.slice(6);
+
   return (
     <div>
-      <h1>Upload Documents</h1>
-      {documentTypes.map((type, index) => (
-        <Upload
-          key={index}
-          name={`file${index}`}
-          action="/api/upload" // Replace with your upload endpoint/ use your backend api to plugin the specific point of uploading
-          fileList={fileList[index] ? [fileList[index]] : []} // Show uploaded file, if any
-          onChange={(info) => handleChange(info, index)}
-          multiple={false}
-        >
-          <Button>{type}</Button>
-        </Upload>
-      ))}
+      <h1 className={styles.Heading}>Upload Documents</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {documentTypesRow1.map((type, index) => (
+          <div key={index} style={{ margin: '10px' }}>
+            <Upload
+              name={`file${index}`}
+              action="/api/upload" // Replace with your upload endpoint/ use your backend api to plugin the specific point of uploading
+              fileList={fileList[index] ? [fileList[index]] : []} // Show uploaded file, if any
+              onChange={(info) => handleChange(info, index)}
+              multiple={false}
+            >
+              <Button>{type}</Button>
+            </Upload>
+          </div>
+        ))}
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {documentTypesRow2.map((type, index) => (
+          <div key={index} style={{ margin: '10px' }}>
+            <Upload
+              name={`file${index + 6}`} // Add an offset to the index to match the correct fileList index
+              action="/api/upload" // Replace with your upload endpoint/ use your backend api to plugin the specific point of uploading
+              fileList={fileList[index + 6] ? [fileList[index + 6]] : []} // Show uploaded file, if any
+              onChange={(info) => handleChange(info, index + 6)} // Add an offset to the index
+              multiple={false}
+            >
+              <Button>{type}</Button>
+            </Upload>
+          </div>
+        ))}
+      </div>
       <Button onClick={handleUpload} disabled={fileList.length !== 11} icon={<UploadOutlined />}>
         Upload
       </Button>
     </div>
   );
+  
 };
 
 export default UploadPage;
