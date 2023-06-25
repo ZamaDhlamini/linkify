7e8c5f12eb0950582880a3a4e8fd231557180778
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-06-25 02:14:59 +0200

    tried to do the banking portion of the web app

---
+++ components/SassaCard/index.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import styles from './SassaCard.module.css';
+import { Button } from 'antd';
+import Link from 'next/link';
+import PaymentPage from '../Payment';
+
+type CreditCard = {
+  cardName: string;
+  cardNumber: string;
+  cvv: string;
+};
+
+const OnlineBankingPage = () => {
+  const [creditCard] = useState<CreditCard>({
+    cardName: 'John Doe',
+    cardNumber: '1234 5678 9012 3456',
+    cvv: '123',
+  });
+
+  const [isPaymentModalOpen, setPaymentModalOpen] = useState(false);
+
+  const handlePaymentButtonClick = () => {
+    setPaymentModalOpen(true);
+  };
+
+  const handlePaymentModalClose = () => {
+    setPaymentModalOpen(false);
+  };
+
+  const handleBuyElectricity = () => {
+    // Logic to handle the purchase of electricity
+    // You can update the balance or perform other actions here
+    console.log('Electricity purchased!');
+  };
+
+  const [balance, setBalance] = useState<number>(350);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Online Banking</h1>
+      <div className={styles.creditCard}>
+        <div className={styles.cardFront}>
+          <div className={styles.cardLogo}></div>
+          <div className={styles.cardChip}></div>
+          <div className={styles.cardNumber}>{creditCard.cardNumber}</div>
+          <div className={styles.cardName}>{creditCard.cardName}</div>
+          <div className={styles.cardValid}></div>
+        </div>
+        <div className={styles.cardBack}>
+          <div className={styles.cardCvv}>{creditCard.cvv}</div>
+        </div>
+      </div>
+      <h2 className={styles.balance}>Balance: R{balance}</h2>
+      <div className={styles.buySection}>
+        <p className={styles.buy}>Buy</p>
+        <p className={styles.electricity}>Electricity</p>
+        <Button onClick={handlePaymentButtonClick}>Buy Electricity</Button>
+        <PaymentPage isOpen={isPaymentModalOpen} onClose={handlePaymentModalClose} />
+      </div>
+      <div className={styles.image}>
+      <img src="/banner.png" alt="Banner box" />
+    </div><div className={styles.imageLogo}>
+          <img src="/coatofarms.png" alt="Banner box" />
+        </div><div className={styles.paragraph1}>
+          <div className={styles.scrollText}>
+          </div>
+          </div>
+    </div>
+  );
+};
+
+export default OnlineBankingPage;
---
+++ pages/Banking/index.tsx
@@ -0,0 +1,9 @@
+import OnlineBanking from "../../components/SassaCard";
+
+const BankingPage = () => {
+    return(
+        <OnlineBanking/>
+    )
+}
+
+export default OnlineBanking;
